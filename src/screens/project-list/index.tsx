ee98715f0a7b947a960d180259593e4ccd750520
liangshengsen <1213509006@qq.com>
2021-03-18 09:45:34 +0800

    chore(project): 添加ts类型定义

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useState, useEffect } from "react";
+import { cleanObject, useDebounce, useMount } from "utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debounceParam = useDebounce(param, 300);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debounceParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,41 +1,43 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const debounce = (func, delay) => {
-  let timeout;
-  return (...params) => {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    timeout = setTimeout(() => {
-      func(...params);
-    }, delay);
-  };
-};
-const log = debounce(() => console.log("log"), 5000);
+// export const debounce = (func, delay) => {
+// 	let timeout;
+// 	return (...params) => {
+// 		if (timeout) {
+// 			clearTimeout(timeout);
+// 		}
+// 		timeout = setTimeout(() => {
+// 			func(...params);
+// 		}, delay);
+// 	};
+// };
+// const log = debounce(() => console.log("log"), 5000);
 // log()
 // log()
 // log()
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     // 每次在value变化后，设置一个定时器
